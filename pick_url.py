be1218c85f4315d7136a5237f0403b2783d88d10
gjsj999 <132079048+gjsj999@users.noreply.github.com>
2023-04-28 23:00:40 +0800

    Create pick_url.py

---
+++ pick_url.py
@@ -0,0 +1,12 @@
+def replaceUrl(new_url):
+    file = "README.md"
+    file_data = ""
+    with open(file, "r", encoding="utf-8") as f:
+        for line in f:
+            if "https" in line:
+                line = line.replace("https",new_url)
+            file_data += line
+    with open(file,"w",encoding="utf-8") as f:
+        f.write(file_data)
+
+updateFile("--[hello world]--")
